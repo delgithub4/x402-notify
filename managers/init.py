4a7c485c86c2123b00b22fe088a0010b314cc95d
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 23:41:41 +0100

    Create init.py

---
+++ managers/init.py
@@ -0,0 +1,5 @@
+from .channel_manager import ChannelManager
+from .delivery_manager import DeliveryManager
+from .notification_manager import NotificationManager
+from .subscriber_manager import SubscriberManager
+from .template_manager import TemplateManager
